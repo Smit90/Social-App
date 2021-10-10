6a84db7cd62643c8dd555607a8408e1726e3bf01
Smit Patel <45687440+Smit90@users.noreply.github.com>
2021-10-10 19:48:40 +0530

    dummyData and like functionality

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,32 +1,42 @@
 import "./post.css"
+import { useState } from "react"
 import { MoreVert } from '@material-ui/icons'
+import { Users } from '../../dummyData'
 
-export default function Post() {
+export default function Post({ post }) {
+    const [like, setLike] = useState(post.like)
+    const [isLike, setIsLike] = useState(false)
+
+    const likeHandler = () => {
+        setLike(isLike ? like - 1 : like + 1)
+        setIsLike(!isLike)
+    }
+
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
-                        <span className="postUsername">Test Dev</span>
-                        <span className="postDate">5 min ago</span>
+                        <img src={Users.filter((u) => u.id === post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                        <span className="postUsername">{Users.filter((u) => u.id === post.userId)[0].username}</span>
+                        <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">It's my first Post</span>
-                    <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+                    <span className="postText">{post?.desc}</span>
+                    <img className="postImg" src={post.photo} alt="" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img src="/assets/like.png" alt="" className="likeIcon" />
-                        <img src="/assets/heart.png" alt="" className="likeIcon" />
-                        <span className="postLikeCounter">32 people liked it</span>
+                        <img src="/assets/like.png" alt="" className="likeIcon" onClick={likeHandler} />
+                        <img src="/assets/heart.png" alt="" className="likeIcon" onClick={likeHandler} />
+                        <span className="postLikeCounter">{like} people like it</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">9 comments</span>
+                        <span className="postCommentText">{post.comment} comments</span>
                     </div>
                 </div>
             </div>
--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -1,5 +1,7 @@
 import "./sidebar.css"
 import { Chat, RssFeed, PlayCircleFilled, Group, Bookmark, HelpOutline, WorkOutline, Event, School } from "@material-ui/icons"
+import { Users } from '../../dummyData'
+import CloseFriend from "../closeFriend/CloseFriend"
 
 export default function SideBar() {
     return (
@@ -46,26 +48,9 @@ export default function SideBar() {
                 <button className="sidebarButton">Show More</button>
                 <hr className="sidebarHr" />
                 <ul className="sidebarFriendList">
-                    <li className="sidebarFriend">
-                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    <li className="sidebarFriend">
-                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    <li className="sidebarFriend">
-                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    <li className="sidebarFriend">
-                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    <li className="sidebarFriend">
-                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
+                    {Users.map((u) => (
+                        <CloseFriend key={u.id} user={u} />
+                    ))}
                 </ul>
             </div>
         </div>
