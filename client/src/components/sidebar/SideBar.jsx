b280eacdde7c5881e272a80f75db48721e7c04dd
Smit Patel <45687440+Smit90@users.noreply.github.com>
2021-10-10 18:02:48 +0530

    sidebar component

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -1,9 +1,73 @@
 import "./sidebar.css"
+import { Chat, RssFeed, PlayCircleFilled, Group, Bookmark, HelpOutline, WorkOutline, Event, School } from "@material-ui/icons"
 
 export default function SideBar() {
     return (
         <div className="sidebar">
-            Side bar
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Chat className="sidebarIcon" />
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PlayCircleFilled className="sidebarIcon" />
+                        <span className="sidebarListItemText">Video</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Group className="sidebarIcon" />
+                        <span className="sidebarListItemText">Group</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Bookmark className="sidebarIcon" />
+                        <span className="sidebarListItemText">Bookmark</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <HelpOutline className="sidebarIcon" />
+                        <span className="sidebarListItemText">Question</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <WorkOutline className="sidebarIcon" />
+                        <span className="sidebarListItemText">Jobs</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Event className="sidebarIcon" />
+                        <span className="sidebarListItemText">Events</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <School className="sidebarIcon" />
+                        <span className="sidebarListItemText">Courses</span>
+                    </li>
+                </ul>
+                <button className="sidebarButton">Show More</button>
+                <hr className="sidebarHr" />
+                <ul className="sidebarFriendList">
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">John Doe</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">John Doe</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">John Doe</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">John Doe</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="sidebarFriendName">John Doe</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
